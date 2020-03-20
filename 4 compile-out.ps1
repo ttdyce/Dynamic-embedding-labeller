1f8f9c621a6b7688a169f6e1063b98325f5dd423
TT <ttdyce@gmail.com>
2020-03-20 21:06:34 +0800

    Update compile-out.ps1 with sorted programs first

--- 4 compile-out.ps1
+++ 4 compile-out.ps1
@@ -1,8 +1,14 @@
 ﻿$dir = "selected-data/"
 $count = 0
-Get-ChildItem $dir -Filter *.cpp | Foreach-Object {
+$max = 10 # set to -1 for all
+Get-ChildItem $dir -Filter *.cpp | Sort-Object LastWriteTime -Descending | Foreach-Object {
     $in = $dir + $_.Name
     $out = $dir + $_.BaseName
     g++ $in -o $out
     "Done: " + ++$count + "(" + $_.Name + ")"
+    $max--
+
+    if ($max -eq 0){
+        break
+    }
 }
