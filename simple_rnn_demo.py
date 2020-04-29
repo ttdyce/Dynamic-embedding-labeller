6e8f9b11fefc534ee9fb1638d508f95401d233a2
TT <ttdyce@gmail.com>
2020-04-29 19:18:07 +0800

    Update rnn demo with Tommy's approach & model 2a

--- simple_rnn_demo.py
+++ simple_rnn_demo.py
@@ -1,58 +1,63 @@
 import collections
 import matplotlib.pyplot as plt
 import numpy as np
-
 import tensorflow as tf
 
-from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
-import DatasetLoader as loader
-from sklearn.preprocessing import LabelBinarizer
+import DatasetLoader as Loader
 
-batch_size = 20
-
-input_dim = 1
-units = 100
+batch_size_fit = 50
+units = 50
 output_size = 3  # labels are from 0 to 3
-epochs = 20
+epochs = 100
+
 
 # Build the RNN model
 def build_model():
-    embedding_layer = layers.Embedding(999+1, output_dim=100)
-    gru_layer = tf.keras.layers.GRU(units) #, input_shape=(None, input_dim)
-
     model = tf.keras.models.Sequential(
         [
-            embedding_layer,
-            gru_layer,
-            # tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Dense(output_size, activation='softmax'),
+            tf.keras.layers.GRU(units, input_shape=(300, 5)),
+            tf.keras.layers.Dense(output_size,activation='softmax')
         ]
     )
-
-    model.summary()
     return model
 
-
-x, y, lens, lenMax, _names, _counts = loader.stateTrace.load()
-
-# x = x.reshape(x.__len__(), lenMax, 1)
-y = LabelBinarizer().fit_transform(y)
+# loader = Loader()
+x, y, lengths, lengthsMax , exeNames, roleInStates = Loader.stateTrace.load(model='2a')
+#x, y, lens, lenMax = loader().loadDefault()
 
 model = build_model()
 
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+opt = tf.keras.optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=opt,
+    metrics=["accuracy"],
+)
 
-x_train, x_test, y_train, y_test = train_test_split(
-    x.astype(int), y.astype(int), test_size=0.1
-)  # replaced by validation_split=0.2
+# x = x.reshape(x.__len__(),300,2)
+print("x.shape",x.shape)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-history = model.fit(x_train, y_train, validation_split=0.1, batch_size=batch_size, epochs=epochs)
+model.summary()
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                            min_delta=0,
+                                            patience=5,
+                                            verbose=0, mode='auto')
 
-print('')
-print('# Evaluate')
+checkpoint = tf.keras.callbacks.ModelCheckpoint('rnn-trace/', monitor='val_accuracy',callbacks=[callback], verbose=1, save_best_only=False, mode='max')
+
+history = model.fit(
+    x_train, y_train, batch_size=batch_size_fit, epochs=epochs, validation_split=0.2
+)
+
+print('\n# Evaluate')
 result = model.evaluate(x_test,y_test)
 dict(zip(model.metrics_names, result))
+
+
+model.save('rnn-trace/', save_format="tf")
+
 
 # draw loss
 plt.plot(history.history["loss"])
