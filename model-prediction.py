3ff52be41ecbf83d11923eff6ec610befc8e05e4
Tommy <tommyhinz15@gmail.com>
2020-05-16 14:59:31 +0800

    adding model2a and 2b plus model prediction code

---
+++ model-prediction.py
@@ -0,0 +1,38 @@
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
+from DatasetLoader import DatasetLoader as Loader
+from scipy import stats
+#'1': Fixed value, '2': Stepper, '3': Gatherer
+
+model = tf.keras.models.load_model('rnn-stateTrace/model1/')
+loader = Loader()
+
+x, y, lens, lenMax = loader.load(datasetPath="out-dataset/dataset-variable-trace-110.npz")
+x = x.reshape(x.__len__(), 1, lenMax)
+print("x shape = ",x.shape)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+print(stats.describe(y_test))
+#for arr in y_test: #do not need the loop at this point, but looks prettier
+    
+p = model.predict(x)
+p = np.argmax(p,axis=-1)
+print(stats.describe(y_test))
+
+# print('\n# Evaluate')
+# result = model.evaluate(x,y)
+# dict(zip(model.metrics_names, result))
+
+# p1 = model.predict(loader.loadPredition(lenMax)[0].reshape(4, 1, lenMax))
+# print("p1 = ",np.argmax(p1,axis=-1), '\n' ,p1)
+
+# p2 = model.predict(loader.loadPredition(lenMax)[1].reshape(4, 1, lenMax))
+# print("p2 = ",np.argmax(p2,axis=-1), '\n' ,p2)
+
+# p3 = model.predict(loader.loadPredition(lenMax)[2].reshape(4, 1, lenMax))
+# print("p3 = ",np.argmax(p3,axis=-1), '\n' ,p3)
---
+++ rnn-model2b.py
@@ -0,0 +1,61 @@
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+import DatasetLoader as Loader
+from sklearn.model_selection import KFold
+
+batch_size_fit = 64
+units = 8
+output_size = 3  # labels are from 0 to 3
+epochs = 650
+
+
+# Build the RNN model
+def build_model():
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.GRU(units, input_shape=(None, 2)),
+            #Dense (units to output_size 3?4?)
+            tf.keras.layers.Dense(output_size,activation='softmax')
+        ]
+    )
+    return model
+
+x, y, lengths, lengthsMax , exeNames, roleInStates = Loader.stateTrace.load(model='1')
+
+model = build_model()
+
+opt = tf.keras.optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=opt,
+    metrics=["accuracy"],
+)
+
+print("x.shape",x.shape)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+model.summary()
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                            min_delta=0,
+                                            patience=5,
+                                            verbose=0, mode='auto')
+
+model.fit(
+    x_train, y_train, batch_size=batch_size_fit, epochs=epochs, validation_split=0.2
+)
+
+print('\n# Evaluate')
+result = model.evaluate(x_test,y_test)
+dict(zip(model.metrics_names, result))
+
+predictionData = Loader.stateTrace.prediction.load(model='1') # same return format with stateTrace.load(model='1')
+
+predictions = model.predict(predictionData[0])
+print(predictions) #result
+print(predictionData[1])
+
+model.save('rnn-stateTrace/model2b/', save_format="tf")
