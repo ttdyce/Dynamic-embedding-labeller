6befb2da99961fb7df4e2c8539ea3c2106b3c925
ttdyce <ttdyce@gmail.com>
2020-05-31 19:10:33 +0800

    Add dicklui version of model 3c

--- rnn-model3c-mod.py
+++ rnn-model3c-mod.py
@@ -8,72 +8,71 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 import DatasetLoader as Loader
 
-tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
-
 # config
-batch_size_fit = 300
-units = 150
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+batch_size_fit = 500
+units = 100
 # output_size = 5  # labels are from 0 to 3
-epochs = 200
+epochs = 300
 
 # dataset
 (x1, x2), y, lengths, lengthsMax, exeNames, roleInStates = Loader.stateTrace.r4mod.load(model='3')
 features = roleInStates.max()
 output_size = y.shape[1]
-# x, y, lens, lenMax = loader().loadDefault()
 
-# model
+####################################model########################################
 first_input = Input(shape=(300, 1))
-d1 = tf.keras.layers.Dense(4)(first_input)
-first_GRU = tf.keras.layers.GRU(units)(d1)
+d11 = tf.keras.layers.Dense(4)(first_input)
+first_GRU = tf.keras.layers.GRU(units)(d11)
 first_dense = tf.keras.layers.Dense(units/2, activation="sigmoid")(first_GRU)
-# ~~softmax~~ -> sigmoid / tanh
 
 second_input = Input(shape=(300, features - 1))
-d2 = tf.keras.layers.Dense((features - 1) * 4)(second_input)
-second_GRU = tf.keras.layers.GRU(units)(d2)
+d21 = tf.keras.layers.Dense((features - 1) * 4)(second_input)
+second_GRU = tf.keras.layers.GRU(units)(d21)
 second_dense = tf.keras.layers.Dense(units/2, activation="sigmoid")(second_GRU)
 
 merged = concatenate([first_dense, second_dense])
-#dense
-#third_dense = tf.keras.layers.Dense(output_size, activation="softmax")(second_GRU)
-#adam 太進取
-merged_dense = tf.keras.layers.Dense(100, activation="relu")(merged) # small dense, see it is 12 or 12 * 5
+merged_dense = tf.keras.layers.Dense(units/2, activation="relu")(merged)
 out = tf.keras.layers.Dense(output_size, activation="softmax")(merged_dense)
 
 model = tf.keras.models.Model(inputs=[first_input, second_input], outputs=out)
+#################################################################################
 
-# try sgd, reduce lr
+#model
+#adam may be too aggresive
 opt = tf.keras.optimizers.Adam(
-    lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False
+    lr=0.0002, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False
 )
-model.compile(loss="categorical_crossentropy", optimizer=tf.keras.optimizers.Adam(lr=0.0001), metrics=["accuracy"])
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
-x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=0.2)
+#training data and validation data
+x1_train, x1_validation, x2_train, x2_validation, y_train, y_validation = train_test_split(x1, x2, y, test_size=0.2)
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1_train, x2_train, y_train, test_size=0.2)
 
 model.summary()
-callback = tf.keras.callbacks.EarlyStopping(
-    monitor="val_loss", min_delta=0, patience=5, verbose=0, mode="auto"
-)
+# callback = tf.keras.callbacks.EarlyStopping(
+#     monitor="val_loss", min_delta=0, patience=5, verbose=0, mode="auto"
+# )
 
 checkpoint = tf.keras.callbacks.ModelCheckpoint(
-    "rnn-trace/",
+    "rnn-stateTrace/model3c_checkpoint/",
     monitor="val_accuracy",
-    callbacks=[callback],
     verbose=1,
-    save_best_only=False,
+    save_best_only=True,
     mode="max",
 )
 
 history = model.fit(
-    [x1_train, x2_train], y_train, batch_size=batch_size_fit, epochs=epochs, validation_split=0.2
+    [x1_train, x2_train], y_train, batch_size=batch_size_fit, epochs=epochs, validation_data=([x1_test, x2_test],y_test)
 )
 
 print("\n# Evaluate")
-result = model.evaluate([x1_test, x2_test], y_test)
+result = model.evaluate([x1_validation, x2_validation], y_validation)
 dict(zip(model.metrics_names, result))
 
-# model.save("rnn-trace/", save_format="tf")
+model.save("rnn-stateTrace/model3c/", save_format="tf")
+
 
 (x1, x2), y, lengths, lengthsMax, exeNames, roleInStates = Loader.stateTrace.r4mod.prediction.load(model='3')
 predictions = model.predict([x1,x2])
@@ -94,8 +93,6 @@ for i in range(y.__len__()):
 print("\n\n# Prediction")
 print(roles, answersCount, correctAnswersCount, sep='\n')
 result = model.evaluate([x1, x2], y)
-
-
 
 # draw loss
 plt.plot(history.history["loss"])
