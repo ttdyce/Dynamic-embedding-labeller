57599f856cd1e586dfd5df049b1662e4fd228efd
ttdyce <ttdyce@gmail.com>
2020-06-02 17:00:05 +0800

    add plot_model to ResultLogger

--- ResultLogger.py
+++ ResultLogger.py
@@ -1,24 +1,28 @@
 import os
 import datetime
+import tensorflow.keras as keras
 
-def log(units, batch_size, epochs, output_size, lr, summary, trainingHistory, evaluationResult, predictionResult, predictionDetails): 
-    logSimply(units, batch_size, epochs, output_size, lr,trainingHistory, evaluationResult, predictionResult, predictionDetails)
-    logWithSummary(units, batch_size, epochs, output_size, lr, summary, trainingHistory, evaluationResult, predictionResult, predictionDetails)
+def plot_model(model, png_name): 
+    keras.utils.plot_model(model, f'log/{png_name}.png')
 
-def logWithSummary(units, batch_size, epochs, output_size, lr, summary, trainingHistory, evaluationResult, predictionResult, predictionDetails): 
+def log(units, batch_size, epochs, output_size, lr, summary, training_history, evaluation_result, prediction_result, prediction_details): 
+    logSimply(units, batch_size, epochs, output_size, lr,training_history, evaluation_result, prediction_result, prediction_details)
+    log_with_summary(units, batch_size, epochs, output_size, lr, summary, training_history, evaluation_result, prediction_result, prediction_details)
+
+def log_with_summary(units, batch_size, epochs, output_size, lr, summary, training_history, evaluation_result, prediction_result, prediction_details): 
     now = datetime.datetime.now()
-    nowStr = now.strftime("%Y-%m-%d %H:%M:%S")
+    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
     
-    log = f'''[{nowStr}] Config: units={units} batch_size={batch_size} epochs={epochs} output_size={output_size} lr={lr}
+    log = f'''[{now_str}] Config: units={units} batch_size={batch_size} epochs={epochs} output_size={output_size} lr={lr}
 # Model summary
 {summary}
 
 # Results
-Training    loss: {trainingHistory.history['loss'][epochs-1]} - accuracy: {trainingHistory.history['accuracy'][epochs-1]}
-            val_loss: {trainingHistory.history['val_loss'][epochs-1]} - val_accuracy: {trainingHistory.history['val_accuracy'][epochs-1]}
-Evaluation loss: {evaluationResult[0]} - accuracy: {evaluationResult[1]}
-Prediction loss: {predictionResult[0]} - accuracy: {predictionResult[1]}
-{predictionDetails}
+Training    loss: {training_history.history['loss'][epochs-1]} - accuracy: {training_history.history['accuracy'][epochs-1]}
+            val_loss: {training_history.history['val_loss'][epochs-1]} - val_accuracy: {training_history.history['val_accuracy'][epochs-1]}
+Evaluation loss: {evaluation_result[0]} - accuracy: {evaluation_result[1]}
+Prediction loss: {prediction_result[0]} - accuracy: {prediction_result[1]}
+{prediction_details}
 ---
 
 '''
@@ -30,17 +34,17 @@ Prediction loss: {predictionResult[0]} - accuracy: {predictionResult[1]}
     with open(filename, "a") as logFile:
         logFile.write(log)
 
-def logSimply(units, batch_size, epochs, output_size, lr, trainingHistory, evaluationResult, predictionResult, predictionDetails): 
+def logSimply(units, batch_size, epochs, output_size, lr, training_history, evaluation_result, prediction_result, prediction_details): 
     now = datetime.datetime.now()
-    nowStr = now.strftime("%Y-%m-%d %H:%M:%S")
+    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
     
-    log = f'''[{nowStr}] Config: units={units} batch_size={batch_size} epochs={epochs} output_size={output_size} lr={lr}
+    log = f'''[{now_str}] Config: units={units} batch_size={batch_size} epochs={epochs} output_size={output_size} lr={lr}
 # Results
-Training    loss: {trainingHistory.history['loss'][epochs-1]} - accuracy: {trainingHistory.history['accuracy'][epochs-1]}
-            val_loss: {trainingHistory.history['val_loss'][epochs-1]} - val_accuracy: {trainingHistory.history['val_accuracy'][epochs-1]}
-Evaluation loss: {evaluationResult[0]} - accuracy: {evaluationResult[1]}
-Prediction loss: {predictionResult[0]} - accuracy: {predictionResult[1]}
-{predictionDetails}
+Training    loss: {training_history.history['loss'][epochs-1]} - accuracy: {training_history.history['accuracy'][epochs-1]}
+            val_loss: {training_history.history['val_loss'][epochs-1]} - val_accuracy: {training_history.history['val_accuracy'][epochs-1]}
+Evaluation loss: {evaluation_result[0]} - accuracy: {evaluation_result[1]}
+Prediction loss: {prediction_result[0]} - accuracy: {prediction_result[1]}
+{prediction_details}
 ---
 
 '''
@@ -50,5 +54,5 @@ Prediction loss: {predictionResult[0]} - accuracy: {predictionResult[1]}
     with open(filename, "a") as logFile:
         logFile.write(log)
     
-# log('units', 'batch_size', 'epochs', 'output_size', 'summary', 'trainingHistory', 'evaluationResult', 'predictionResult', 'predictionDetails')
+# log('units', 'batch_size', 'epochs', 'output_size', 'summary', 'training_history', 'evaluation_result', 'prediction_result', 'prediction_details')
     
